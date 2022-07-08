13a020456469266ad5909498424438c2c1bca271
Mohamed Merrouch <m.merrouch99@gmail.com>
2022-07-08 10:06:33 +0100

    feat: Pokemon specs component

---
+++ src/components/PokemonSpecs.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PokemonSpecs = () => {
+  return (
+    <div className="bg-blue-300">
+      <div className="column">
+        <ul>
+          <li>Height: 3 03</li>
+          <li>Weight: 49.6 lbs</li>
+          <li>Gender: Male</li>
+        </ul>
+      </div>
+      <div className="column">
+        <ul>
+          <li>Category: Turtle</li>
+          <li>Abilities: Torrent</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonSpecs;
