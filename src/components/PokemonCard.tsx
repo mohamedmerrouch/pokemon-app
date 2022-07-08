9c50018bca6ea47ef9352db53abe609a3908ed7e
Mohamed Merrouch <m.merrouch99@gmail.com>
2022-07-08 10:05:59 +0100

    feat: Pokemon card component

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TypeBadge from "./TypeBadge";
+
+const PokemonCard: React.FC = () => {
+  return (
+    <div className="bg-blue-700 basis-1/3 flex-none">
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png"
+        alt="Charizard"
+      />
+      <p>#006</p>
+      <p>Charizard</p>
+      <TypeBadge />
+    </div>
+  );
+};
+
+export default PokemonCard;
