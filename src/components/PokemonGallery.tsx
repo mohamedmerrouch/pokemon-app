ed7ea247e664ed5f43f48ace7dd8338e576f306d
Mohamed Merrouch <m.merrouch99@gmail.com>
2022-07-08 10:06:15 +0100

    feat: Pokemon gallery component

---
+++ src/components/PokemonGallery.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+
+const PokemonGallery = () => {
+  return (
+    <div className="container flex flex-row flex-wrap justify-around bg-yellow-300">
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+      <PokemonCard />
+    </div>
+  );
+};
+
+export default PokemonGallery;
