349a722e496031a7f03346fa214489d887d5f0e7
Mohamed Merrouch <m.merrouch99@gmail.com>
2022-07-08 12:14:45 +0100

    feat: google fonts poppins + styling PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -3,14 +3,22 @@ import TypeBadge from "./TypeBadge";
 
 const PokemonCard: React.FC = () => {
   return (
-    <div className="bg-blue-700 basis-1/3 flex-none">
+    <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-2 transform transition duration-500 hover:scale-105">
       <img
+        className="rounded-t-lg w-full object-cover"
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png"
-        alt="Charizard"
+        alt=""
       />
-      <p>#006</p>
-      <p>Charizard</p>
-      <TypeBadge value="ground" />
+      <div className="p-5">
+        <h6 className="mb-0 text-sm font-normal tracking-tight text-gray-500 dark:text-white">
+          #006
+        </h6>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Charizard
+        </h5>
+        <TypeBadge value="ground" />
+        <TypeBadge value="fire" />
+      </div>
     </div>
   );
 };
